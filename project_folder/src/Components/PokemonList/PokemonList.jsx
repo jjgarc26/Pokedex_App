af77fa16e99c1707b1afe5d7faa2a388374a7b42
Jose Garcia <mendoza.joser51@gmail.com>
2022-03-10 19:50:06 -0700

    added preview component

--- project_folder/src/Components/PokemonList/PokemonList.jsx
+++ project_folder/src/Components/PokemonList/PokemonList.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import PokemonPreview from "../PokemonPreview/PokemonPreview";
 
+//default data for preview
+const defaultPreview = {
+  name: "Bulbasaur",
+  id: 1,
+  image:
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+};
 const PokemonList = (props) => {
+  const [previewData, setPreviewData] = useState(defaultPreview);
+  const previewHandler = (id, name, image) => {
+    setPreviewData({ id: id, name: name, image: image });
+  };
   const pokemonList = props.pokemonData.map((pokemon) => {
     return (
-      <li key={pokemon.id}>
+      <li
+        key={pokemon.id}
+        onClick={() =>
+          previewHandler(
+            pokemon.id,
+            pokemon.name,
+            pokemon.sprites.other["official-artwork"].front_default
+          )
+        }
+      >
         <div>{pokemon.id}</div>
         <div>
           <img src={pokemon.sprites.front_default} alt="pokemon sprite" />
@@ -15,7 +36,9 @@ const PokemonList = (props) => {
 
   return (
     <React.Fragment>
-      <div></div>
+      <div>
+        <PokemonPreview previewInfo={previewData} />
+      </div>
       <div>
         <ul>
           <div>{pokemonList}</div>
--- project_folder/src/Components/PokemonPreview/PokemonPreview.jsx
+++ project_folder/src/Components/PokemonPreview/PokemonPreview.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-const defaultPreview = {
-  name: "Bulbasaur",
-  id: 1,
-};
-const PokemonPreview = () => {
-  const [preview, setPreview] = useState(defaultPreview);
-
+import React from "react";
+const PokemonPreview = (props) => {
+  const name = props.previewInfo.name;
+  const id = props.previewInfo.id;
+  const image = props.previewInfo.image;
   return (
     <React.Fragment>
-      <div>{preview.id}</div>
-      <div>{preview.name}</div>
+      <div>{name}</div>
+      <div>{id}</div>
+      <div>
+        <img src={image} alt="pokemon artwork" />
+      </div>
     </React.Fragment>
   );
 };
