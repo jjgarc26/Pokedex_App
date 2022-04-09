18b511037e4592751496a34dca1850ef3318f04a
Jose Garcia <mendoza.joser51@gmail.com>
2022-04-09 14:04:04 -0700

    added more css and select option

--- project_folder/src/App.js
+++ project_folder/src/App.js
@@ -1,30 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import PokemonApi from "./api/PokemonAPI";
 import PokemonInformation from "./Components/PokemonInformation/PokemonInformation";
 import styles from "./App.module.css";
 import Card from "./UI/Card/Card";
 import mainLogo from "./Images/pokemon_logo.png";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "Kanto":
+      return { ...state, name: action.payload, limit: "150" };
+    case "Johto":
+      return { ...state, name: action.payload, limit: "250" };
+    case "Shino":
+      return { ...state, name: action.payload, limit: "150" };
+    default:
+      return { ...state, name: action.payload, limit: "150" };
+  }
+};
+
 function App() {
   const [showList, setShowList] = useState(false);
   const [option, setOption] = useState("list");
   const [mainData, setMainData] = useState({});
+  const [state, dispatch] = useReducer(reducer, {
+    name: "Kanto",
+    limit: "150",
+  });
   const showListHandler = () => {
     setShowList(true);
   };
   const getMainPokemon = (data) => {
     console.log(data);
   };
-  const defaultView = () => {
+  const setRegionHandler = (event) => {
+    dispatch({
+      type: event.target.value,
+      payload: event.target.value,
+    });
+  };
+  const mainMenu = () => {
     return (
       <Card>
+        <div>
+          <h3>Select region</h3>
+          <select onChange={setRegionHandler}>
+            <option value="Kanto">Kanto</option>
+            <option value="Johto">Johto</option>
+            <option value="Shino">Shino</option>
+          </select>
+        </div>
         <div className={styles.mainLogo}>
-          <img src={mainLogo} alt="main pokemon logo" />
+          <img
+            className={styles.titleImage}
+            src={mainLogo}
+            alt="main pokemon logo"
+          />
         </div>
         <div className={styles.appH2}>
-          <h2>Pokedex</h2>
+          <h2>
+            {state.name} <br /> Pokedex
+          </h2>
         </div>
         <div className={styles.appButton}>
-          <button onClick={showListHandler}>Click to Begin</button>
+          <button onClick={showListHandler}>Begin Exploring</button>
         </div>
       </Card>
     );
@@ -34,7 +72,7 @@ function App() {
   };
   return (
     <div>
-      {!showList ? defaultView() : <PokemonApi mainData={getMainPokemon} />}
+      {!showList ? mainMenu() : <PokemonApi mainData={getMainPokemon} />}
     </div>
   );
 }
