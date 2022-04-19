39fa12ebf2996b2bacc149f189394f9ed8914194
Jose Garcia <mendoza.joser51@gmail.com>
2022-04-18 20:48:26 -0700

    added more styles

--- project_folder/src/Components/PokemonPreview/PokemonPreview.jsx
+++ project_folder/src/Components/PokemonPreview/PokemonPreview.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import styles from "./PokemonPreview.module.css";
 const PokemonPreview = (props) => {
   const pokemon = {
     name: props.previewInfo.name,
     id: props.previewInfo.id,
     image: props.previewInfo.image,
+    capName: function () {
+      return this.name.charAt(0).toUpperCase() + this.name.slice(1);
+    },
   };
   return (
-    <React.Fragment>
-      <div>{pokemon.name}</div>
-      <div>{pokemon.id}</div>
-      <div>
+    <div className={styles.previewMain}>
+      <div className={styles.previewId}>{pokemon.id}</div>
+      <div className={styles.previewImage}>
         <img src={pokemon.image} alt="pokemon artwork" />
       </div>
-    </React.Fragment>
+      <div className={styles.previewName}>{pokemon.capName()}</div>
+    </div>
   );
 };
 export default PokemonPreview;
